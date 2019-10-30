d3555dfa11177a6d6380efb09312c569acba4761
Terje Sannum <terje.sannum@nav.no>
2019-10-30 07:24:54 +0100

    add delete-orphaned-hpas

---
+++ delete-orphaned-hpas.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/env perl
+
+use JSON;
+
+$dryrun = $ARGV[0] =~ /dryrun/;
+
+$|=1;
+$json = JSON->new;
+
+$j = `kubectl get hpa --all-namespaces -o json`;
+$hpas = $json->decode($j);
+foreach $hpa (@{$hpas->{items}}) {
+    next if($hpa->{spec}->{scaleTargetRef}->{kind} ne 'Deployment');
+    $name = $hpa->{metadata}->{name};
+    $namespace = $hpa->{metadata}->{namespace};
+    $deploy = $hpa->{spec}->{scaleTargetRef}->{name};
+    $get = `kubectl -n $namespace get deploy $deploy 2>&1`;
+    if($get =~ /not found/) {
+        print "kubectl -n $namespace delete hpa $name\n";
+        print `kubectl -n $namespace delete hpa $name` unless($dryrun);
+    }
+}
