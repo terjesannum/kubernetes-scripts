d9e2cad22e3b3b8ca7937f7e903ee02a3ad93eb8
Terje Sannum <terje.sannum@nav.no>
2020-02-14 11:45:10 +0100

    use Time:HiRes

--- hostname-lookup.pl
+++ hostname-lookup.pl
@@ -1,6 +1,7 @@
 #!/usr/bin/env perl
 
 use Socket;
+use Time::HiRes qw(usleep ualarm);
 
 die "Usage: $0 <host> [sleep] [stats]\n" unless $#ARGV >= 0;
 
@@ -8,22 +9,28 @@ $| = 1;
 $host = $ARGV[0];
 $sleep = $ARGV[1] // 1000;
 $stats = $ARGV[2] // 1000;
-$timeout = $ARGV[3] // 2;
+$timeout = $ARGV[3] // 1000;
 
 $SIG{INT} = $SIG{TERM} = \&signal;
-$SIG{ALRM} = \&alarm;
 
-printf("Starting hostname lookup of %s, pause %dms, timeout %ds%s\n",
+printf("Starting hostname lookup of %s, pause %dms, timeout %dms%s\n",
        $host, $sleep, $timeout, $stats ? sprintf(", stats every %d lookup.", $stats) : ".");
 
 while(1) {
-    alarm($timeout);
-    $ip = gethostbyname($host);
-    alarm(0);
-    $ip = $ip ? inet_ntoa($ip) : "n/a";
+    eval {
+        local $SIG{ALRM} = sub { die "alarm\n" };
+        ualarm($timeout * 1000);
+        $ip = gethostbyname($host);
+        ualarm(0);
+    };
+    if($@) {
+        $ip = $@ eq "alarm" ? "timeout" : "fail";
+    } else {
+        $ip = $ip ? inet_ntoa($ip) : "n/a";
+    }
     $results->{$ip}++;
     &print_stats if($stats && ++$count % $stats == 0);
-    select(undef, undef, undef, $sleep / 1000);       
+    usleep($sleep * 1000);
 }
 
 sub print_stats {
@@ -33,10 +40,6 @@ sub print_stats {
     printf("%d total\n", $count);
 }
             
-sub alarm {
-    $results->{"timeout"}++;
-}
-
 sub signal {
     print "Lookup results:\n";
     &print_stats;
